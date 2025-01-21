a699259f0706473acb96b88c63e9e2770cd08775
tony <tonyantony300@gmail.com>
2025-01-21 11:36:04 +0530

    Desktop no vault add cred fix

--- frontends/src/desktop/components/ui/VaultSelector.svelte
+++ frontends/src/desktop/components/ui/VaultSelector.svelte
@@ -2,7 +2,7 @@
 	import { fly } from "svelte/transition";
 	import MobileHome from "../../../icons/mobileHome.svelte";
 	import { vaults, currentVault } from "../store/desktop.ui.store";
-	import { createEventDispatcher } from "svelte";
+	import { createEventDispatcher, onMount } from "svelte";
 
 	const dispatch = createEventDispatcher();
 
@@ -16,16 +16,22 @@
 	style="top: calc(100% + 10px)"
 	id="vaultSelector"
 	in:fly>
-	{#each $vaults.filter((vault) => vault.id !== "all") as vault (vault.id)}
-		{@const isActive = $currentVault.id === vault.id}
-		<button
-			class="h-[48px] p-4 text-mobile-textPrimary flex items-center rounded-lg hover:bg-osvauld-frameblack"
-			class:bg-mobile-bgLight="{isActive}"
-			class:text-mobile-textTertiary="{isActive}"
-			on:click|stopPropagation="{() => handleVaultSelection(vault)}">
-			<span><MobileHome color="{isActive ? '#F2F2F0' : '#85889C'}" /></span>
-			<span class="grow text-left pl-2 capitalize max-w-full truncate"
-				>{vault.name}</span>
-		</button>
-	{/each}
+	{#if $vaults.length === 1}
+		<span
+			class="text-osvauld-dangerRed text-lg h-10 w-full p-2 flex justify-center items-center"
+			>Please Add Vault to Proceed</span>
+	{:else}
+		{#each $vaults.filter((vault) => vault.id !== "all") as vault (vault.id)}
+			{@const isActive = $currentVault.id === vault.id}
+			<button
+				class="h-[48px] p-4 text-mobile-textPrimary flex items-center rounded-lg hover:bg-osvauld-frameblack"
+				class:bg-mobile-bgLight="{isActive}"
+				class:text-mobile-textTertiary="{isActive}"
+				on:click|stopPropagation="{() => handleVaultSelection(vault)}">
+				<span><MobileHome color="{isActive ? '#F2F2F0' : '#85889C'}" /></span>
+				<span class="grow text-left pl-2 capitalize max-w-full truncate"
+					>{vault.name}</span>
+			</button>
+		{/each}
+	{/if}
 </div>
