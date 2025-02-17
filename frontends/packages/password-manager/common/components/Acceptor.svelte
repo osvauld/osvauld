252f2a4b0d7a14c5f5118ecb6ba4ec39e5e32ebf
Abraham George <abrahamgeorge8547@gmail.com>
2025-02-17 16:28:03 +0530

    sync component mounted

--- frontends/packages/password-manager/common/components/Acceptor.svelte
+++ frontends/packages/password-manager/common/components/Acceptor.svelte
@@ -56,7 +56,8 @@
 	}
 </script>
 
-<div class="p-4 z-[999] flex flex-col gap-4">
+<div
+	class="p-4 inset-0 items-center justify-center z-50 bg-osvauld-backgroundBlur backdrop-filter backdrop-blur-[2px] fixed flex flex-col gap-4">
 	<div class="bg-mobile-bgSeconary rounded-lg p-4">
 		<h2 class="text-xl mb-2 text-mobile-textPrimary">Receive Connection</h2>
 		<p class="text-mobile-textSecondary mb-4">
