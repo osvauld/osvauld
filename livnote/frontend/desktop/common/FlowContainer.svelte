d9ea3dc6290771efa3091a0a4a6ea67d4fa917d4
tony <tonyantony300@gmail.com>
2025-08-20 20:41:55 +0530

    Accomodations for verying horzontal viewport height-2

--- livnote/frontend/desktop/common/FlowContainer.svelte
+++ livnote/frontend/desktop/common/FlowContainer.svelte
@@ -15,7 +15,7 @@
 	class="grow flex flex-col justify-center items-center"
 >
 	<button
-		class="absolute top-1/2 left-0 -translate-y-1/2 cursor-pointer border border-transparent focus:border-livnotePink outline-0 rounded-lg p-1"
+		class="absolute top-16 left-6 cursor-pointer border border-transparent focus:border-livnotePink outline-0 rounded-lg p-1"
 		onmouseenter={() => (goBackHover = true)}
 		onmouseleave={() => (goBackHover = false)}
 		onclick={onBack}
