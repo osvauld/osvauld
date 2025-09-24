d0152e4e562c8c75537b314c0d7543f6f3df860f
tony <tonyantony300@gmail.com>
2025-09-24 22:29:55 +0530

    make left navpanel unconditional & smaller preview

--- livnote/frontend/desktop/components/notes/NotesListView.svelte
+++ livnote/frontend/desktop/components/notes/NotesListView.svelte
@@ -29,51 +29,26 @@
 	};
 </script>
 
-<div class="grow max-h-full overflow-hidden px-4 py-0.5 relative flex flex-col">
-	<NoteListPanel />
-	<div class="grow pr-1 scrollbar-thin min-w-[37.5rem] overflow-y-auto">
-		{#if dataState.isDataLoading}
-			<div class="flex justify-center items-center h-full">
-				<div class="text-osvauld-fieldText">Loading notes...</div>
-			</div>
-		{:else if dataState.filteredNotes.length === 0}
-			<div class="flex justify-center items-center h-full">
-				<div class="text-osvauld-fieldText">
-					{dataState.favoriteSelected
-						? "No favourites found."
-						: "No notes found. Create a new note to get started."}
-				</div>
-			</div>
-		{:else}
-			<div
-				class="grid gap-6"
-				style="grid-template-columns: repeat(auto-fit, minmax(500px, 500px)); justify-content: start;"
-			>
-				{#each dataState.filteredNotes as note (note.id)}
-					<div
-						role="presentation"
-						class="w-[500px] bg-osvauld-frameblack border border-osvauld-borderColor rounded-lg overflow-hidden hover:border-livnotelavender transition-colors duration-200 cursor-pointer"
-						onclick={() => selectNote(note)}
-					>
-						<div
-							class="p-4 border-b border-osvauld-borderColor flex justify-between items-center"
-						>
-							<h3 class="text-osvauld-fieldText font-medium text-lg truncate">
-								{note?.title}
-							</h3>
-							<button
-								class="flex items-center justify-center p-1 cursor-pointer"
-								onclick={(e) => {
-									e.stopPropagation();
-									toggleFavorite(note.id, note.favourite ?? false);
-								}}
-							>
-								{#if note.favourite}
-									<Star />
-								{:else}
-									<EmptyStar color="#85889C" />
-								{/if}
-							</button>
+<div class="flex grow max-h-full max-w-full">
+	<NavigationPanel />
+
+	<div class="flex-1 flex flex-col overflow-hidden">
+		<div
+			class="grow max-h-full overflow-hidden px-11 py-4 relative flex flex-col"
+		>
+			<NoteListPanel />
+
+			<div class="grow pr-1 scrollbar-thin min-w-[37.5rem] overflow-y-auto">
+				{#if dataState.isDataLoading}
+					<div class="flex justify-center items-center h-full">
+						<div class="text-osvauld-fieldText">Loading notes...</div>
+					</div>
+				{:else if dataState.filteredNotes.length === 0}
+					<div class="flex justify-center items-center h-full">
+						<div class="text-osvauld-fieldText">
+							{dataState.favoriteSelected
+								? "No favourites found."
+								: "No notes found. Create a new note to get started."}
 						</div>
 					</div>
 				{:else}
